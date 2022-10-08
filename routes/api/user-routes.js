f561b15ae16ecdca8cd77a9f6f88a77f19f9d481
stephen <94320530+stephen-kern@users.noreply.github.com>
2022-10-08 11:54:30 -0500

    verify password function

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -35,7 +35,7 @@ router.get("/:id", (req, res) => {
     });
 });
 
-// POST /api/users
+// POST /api/users creates new user
 router.post("/", (req, res) => {
   // expects {username:'', email: '', password: ''}
   User.create({
@@ -48,6 +48,27 @@ router.post("/", (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.post('/login', (req, res) => {
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address' });
+      return;
+    }
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if(!validPassword) {
+      res.status(400).json({ message: 'Incorrect password' });
+      return;
+    }
+
+    res.json({ user: dbUserData, message: 'You are now logged in' });
+
+  })
 });
 
 // PUT /api/users/1
