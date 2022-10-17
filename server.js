2d25971ecef2bb51c256c51c05443a5e36a68351
stephen <94320530+stephen-kern@users.noreply.github.com>
2022-10-16 20:56:24 -0500

    created test for date, url, and plural words

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const path = require("path");
 const exphbs = require("express-handlebars");
-const hbs = exphbs.create({});
-const session = require('express-session')
+const hbs = exphbs.create({ helpers });
+const session = require('express-session');
+const helpers = require('./utils/helpers');
 
 // SET UP EXPRESS SESSION AND CONNECT TO SEQUELIZE DATABASE
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
